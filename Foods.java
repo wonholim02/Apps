1e8bf031cdddac891e11f53326ff23814a0d34f5
Wonho Lim <62059099+wonholim02@users.noreply.github.com>
2020-04-25 14:11:19 +0900

    Add files via upload

---
+++ Foods.java
@@ -0,0 +1,21 @@
+package com.example.mobilesanitizer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+public class Foods extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_foods);
+    }
+    public void openz(View view){
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.who.int/news-room/fact-sheets/detail/food-safety"));
+        startActivity(browserIntent);
+    }
+}
