cde8a24c2a19139f7c1d40473de12e42627f5ec2
Wonho Lim <62059099+wonholim02@users.noreply.github.com>
2020-07-05 13:46:34 +0900

    Add files via upload

---
+++ NumbersClickListener.java
@@ -0,0 +1,10 @@
+package com.example.koreanapp;
+
+import android.view.View;
+import android.widget.Toast;
+
+public class NumbersClickListener implements View.OnClickListener {
+    public void onClick(View view){
+        Toast.makeText(view.getContext(), "Open the list of numbers", Toast.LENGTH_SHORT).show();
+    }
+}
