57ea652ada5ed8abeccc74412c7cbd94f6359d57
Wonho Lim <62059099+wonholim02@users.noreply.github.com>
2020-07-05 13:47:55 +0900

    Add files via upload

---
+++ sec2.java
@@ -0,0 +1,14 @@
+package com.example.koreanapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class sec2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sec2);
+    }
+}
